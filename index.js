36ddd4f08453ac8aa6f2d8ed33bab49cdf61943e
ahmadhafidh <ahmad.hafidhayatullah@gmail.com>
2022-04-01 14:45:08 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log("Latihan Node JS Awal")
+
+// #1
+// const os = require('os')
+// const luasSegitiga = require('./segitiga')
+
+// console.log("Free Memory:", os.freemem())
+// console.log(luasSegitiga(3,4))
+
+// #3 Write File
+// const fs = require('fs')
+// fs.writeFileSync('./text.txt', 'I Love Binar Academy')
+
+// #2 Read File
+// const fs = require('fs')
+// const isi = fs.readFileSync('./text.txt', 'utf-8')
+// console.log(isi)
+
+// #4 Write File
+// const fs = require('fs')
+// const isi = fs.readFileSync('./text.txt', 'utf-8')
+// const sabrina = require('./person.json')
+// console.log(sabrina)
+
+const create = require("./create.js")
+const sabrina = require("./person.json")
+
+// console.log(create)
+
+
+
---
+++ segitiga.js
@@ -0,0 +1,6 @@
+function luasSegitiga(alas, tinggi){
+    let data = alas * tinggi / 2
+    return data
+}
+
+module.exports = luasSegitiga
